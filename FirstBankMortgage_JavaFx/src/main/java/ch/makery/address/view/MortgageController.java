328f32cfc206f6ea63f296747c482296da1564c6
Nihar <Nihar@10.0.0.32>
2015-12-18 23:01:54 -0500

    Final Commit

--- FirstBankMortgage_JavaFx/src/main/java/ch/makery/address/view/MortgageController.java
+++ FirstBankMortgage_JavaFx/src/main/java/ch/makery/address/view/MortgageController.java
@@ -1,11 +1,15 @@
 package ch.makery.address.view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.util.UUID;
 
@@ -15,7 +19,26 @@ import ch.makery.address.model.Rate;
 
 
 public class MortgageController {
+@FXML
+private TextField txtIncome;
+@FXML
+private TextField txtExpenses;
+@FXML
+private TextField txtCreditScore;
+@FXML
+private TextField txtHouseCost;
+@FXML
+private ComboBox cmbTerm;
+@FXML
+private Label lblMortgagePayment;
 
+@FXML
+ObservableList<String> options = 
+FXCollections.observableArrayList(
+    "15",
+    "30"
+);
+final ComboBox comboBox = new ComboBox(options);
 
     // Reference to the main application.
     private MainApp mainApp;
